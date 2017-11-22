90f25afea7901cabb5f97e9b0f465e7512da1726
batmangoodsen <1281198374@qq.com>
2017-11-22 09:29:25 +0800

    修复产品栏定位不到问题(先点击产品栏,再输入内容)

--- testSelenium/searchParament.py
+++ testSelenium/searchParament.py
@@ -6,6 +6,7 @@ for text in search_text:
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)
     driver.get("http://casicloud.com/search?q=")
+    driver.find_element_by_xpath("html/body/div[2]/div[2]/div/div[1]/a[1]").click()
     driver.find_element_by_xpath(".//*[@id='keyword']").send_keys(text)
     driver.find_element_by_xpath("html/body/div[2]/div[2]/div/div[2]/a").click()
     driver.quit()
