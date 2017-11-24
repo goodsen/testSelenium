0352ce4032e19ed52f50a21e65ffac458493ad9f
batmangoodsen <1281198374@qq.com>
2017-11-24 18:39:14 +0800

    使用unittest进行单元测试

--- caculatorTest.py
+++ caculatorTest.py
@@ -5,12 +5,22 @@ class TestCount(unittest.TestCase):
     #定义启动
     def setUp(self):
         print("test start")
-    #定义单元测试
+    #定义测试用例1
     def test__add(self):
         j = Count(2,3)
         self.assertEqual(j.add(),5)#增加断言方法
+    #定义测试用例2
+    def test_add2(self):
+        j = Count(41,76)
+        self.assertAlmostEquals(j.add(),117)
     def tearDown(self):
         print("test end")
 if __name__ == '__main__':
-    unittest.main()
+    # 构建测试集
+    suite = unittest.TestSuite()
+    suite.addTest(TestCount("test_add2"))
+    #执行测试
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
 
