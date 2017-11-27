e197bb3b6a97375d05646fe30c3d6eec1042091a
batmangoodsen <1281198374@qq.com>
2017-11-27 17:49:58 +0800

    修改掉包方式和调用方法

--- testpro/runtest.py
+++ testpro/runtest.py
@@ -1,16 +1,16 @@
 import  unittest
 
 #加载测试文件
-import testpro.testadd
-import testpro.testsub
+from testpro import testadd
+from testpro import testsub
 
 #构造测试集
 suite = unittest.TestSuite()
-suite.addTest(testpro.testadd("test_add"))
-suite.addTest(testpro.testsub("test_add2"))
+suite.addTest(testadd.TestAdd("test_add"))
+suite.addTest(testadd.TestAdd("test_add2"))
 
-suite.addTest(testpro.testsub("test_add"))
-suite.addTest(testpro.testsub("test_add2"))
+suite.addTest(testsub.TestSub("test_add"))
+suite.addTest(testsub.TestSub("test_add2"))
 
 if __name__ == '__main__':
     #执行测试
