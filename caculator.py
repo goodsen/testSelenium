30d2175639793efdecd0b1e88c9e3b69dbf268e6
batmangoodsen <1281198374@qq.com>
2017-11-27 16:45:03 +0800

    完善运算：增加乘法、除法、减法运算

--- caculator.py
+++ caculator.py
@@ -7,3 +7,13 @@ class Count:
 #计算加法
     def add(self):
         return self.a+self.b
+#计算减法
+    def sub(self):
+        return self.a - self.b
+
+#计算乘法
+    def mul(self):
+        return self.a * self.b
+#计算除法
+    def div(self):
+        return self.a / self.b
