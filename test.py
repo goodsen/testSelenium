8d23b6eebbcdd486d239c0fee3d36897c80fba65
batmangoodsen <1281198374@qq.com>
2017-11-27 17:04:19 +0800

    增加MyTest()类将注意力放在具体用例编写上，无须关心setUp、tearDown()所做的事情。但是，前提是setUp、tearDown()所做的事情是setUp、tearDown()所做的事情是每个用例都需要的

--- test.py
+++ test.py
@@ -1,35 +1,25 @@
 from caculator import Count
 import unittest
 
-class TestAdd(unittest.TestCase):
+class MyTest(unittest.TestCase):
     def setUp(self):
-        print("test add start")
+        print("测试开始")
+    def tearDown(self):
+        print("测试结束")
+
+class TestAdd(MyTest):
     def test_add(self):
         j = Count(2,3)
         self.assertEqual(j.add(),5)
     def test_add2(self):
         j = Count(41,76)
         self.assertEqual(j.add(),117)
-    def tearDown(self):
-        print("测试结束，加法没有错误")
-class TestSub(unittest.TestCase):
-    def setUp(self):
-        print("test sub start")
+class TestSub(MyTest):
     def test_sub(self):
         j = Count(2,3)
         self.assertEqual(j.sub(),-1)
     def test_sub2(self):
         j = Count(71,46)
         self.assertEqual(j.sub(),25)
-    def tearDown(self):
-        print("测试结束，减法没有报错")
 if __name__ == '__main__':
-    #构造测试类
-    suite = unittest.TestCase()
-    suite.addTest(TestAdd("test_add"))
-    suite.addTest(TestAdd("test_add2"))
-    suite.addTest(TestSub("test_add"))
-    suite.addTets(TestSub("test_add2"))
-    #运行测试集合
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    unittest.main()
