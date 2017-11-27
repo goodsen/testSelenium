97214dc0ad3ef42c8483ff4f7554ea1bf7a455b8
batmangoodsen <1281198374@qq.com>
2017-11-27 16:46:19 +0800

    增加测试类、运行测试集合

--- test.py
+++ test.py
@@ -11,4 +11,25 @@ class TestAdd(unittest.TestCase):
         j = Count(41,76)
         self.assertEqual(j.add(),117)
     def tearDown(self):
-        print("测试结束，没有错误")
+        print("测试结束，加法没有错误")
+class TestSub(unittest.TestCase):
+    def setUp(self):
+        print("test sub start")
+    def test_sub(self):
+        j = Count(2,3)
+        self.assertEqual(j.sub(),-1)
+    def test_sub2(self):
+        j = Count(71,46)
+        self.assertEqual(j.sub(),25)
+    def tearDown(self):
+        print("测试结束，减法没有报错")
+if __name__ == '__main__':
+    #构造测试类
+    suite = unittest.TestCase()
+    suite.addTest(TestAdd("test_add"))
+    suite.addTest(TestAdd("test_add2"))
+    suite.addTest(TestSub("test_add"))
+    suite.addTets(TestSub("test_add2"))
+    #运行测试集合
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
