f442ce7b028b4239c633bc621564a39fd51f7c01
batmangoodsen <1281198374@qq.com>
2017-11-28 17:18:38 +0800

    尝试测试类和模块的fixtures

---
+++ testFixtures.py
@@ -0,0 +1,26 @@
+import unittest
+
+#设置用例启动方法
+def setUpModule():
+    print("test module start >>>>>>>>>>>>>")
+#设置用例结束方法
+def tearDownModule():
+    print("test module end   >>>>>>>>>>>>>")
+#Test单元测试类
+class Test(unittest.TestSuite):
+    @classmethod
+    def setUpClass(cls):
+        print("test class start======>")
+    @classmethod
+    def tearDownClass(cls):
+        print("test class end =======>")
+    def setUp(self):
+        print("test case start-->")
+    def tearDown(self):
+        print("test case end  -->")
+    def test_case(self):
+        print("test case1")
+    def test_case2(self):
+        print("test case2")
+if __name__ == '__main__':
+    unittest.main()
