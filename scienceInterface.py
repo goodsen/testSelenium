f7b0c74741f80b00d5f46d94ea98b64f80e32662
batmangoodsen <1281198374@qq.com>
2017-11-29 17:51:33 +0800

    接口自动化测试requests+unittest

---
+++ scienceInterface.py
@@ -0,0 +1,27 @@
+import requests
+import unittest
+
+#定义接口测试类
+class scienceInterface(unittest.TestCase):
+    #定义启动方法
+    def setUp(self):
+        self.url = ""
+    def test_get_event_null(self):
+        r = requests.get(self.url,params={'key':'value'})
+        result = r.json()
+        print(result)
+        self.assertEqual(result['status'],10021)
+        self.assertEqual(result['message'],"parameter error")
+    def test_get_event_success(self):
+        r = requests.get(self.url,params={'eid':'1'})
+        result = r.json()
+        print(result)
+        self.assertEqual(result['status'],200)
+        self.assertEqual(result['message',"success"])
+        self.assertEqual(result['data']['name'],"xx产品发布会")
+        self.assertEqual(result['data']['address'],"北京奥林匹克公园水立方")
+        #self.assertEqual(result['data'])['start_time',"2016-10-15T18:00:00"]
+    def tearDown(self):
+        print("结束测试")
+if __name__ == '__main__':
+    unittest.main()
