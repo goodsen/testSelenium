a41ecdfa5084c7f67f1865767389fb0c5d413eb5
batmangoodsen <1281198374@qq.com>
2017-12-01 14:22:19 +0800

    修改搜索框id错误

--- testSelenium/test_baidu.py
+++ testSelenium/test_baidu.py
@@ -10,7 +10,7 @@ class Baidu(unittest.TestCase):
     def test_baidu_search(self):
         driver = self.driver
         driver.get(self.base_url)
-        driver.find_element_by_id("Kw").send_keys("HTMLTestRunner")
+        driver.find_element_by_id("kw").send_keys("HTMLTestRunner")
         driver.find_element_by_id("su").click()
         time.sleep(2)
         driver.close()
@@ -22,11 +22,11 @@ if __name__ == "__main__":
 
 
     #定义报告存放路径
-    fp = open(r'E:\py3','wb')
-    #file_result = open(file_path ,'wb')
+    fp = "E:\\result.html"
+    fp_result = open(fp ,'wb')
 
     #定义测试报告
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title="百度搜索测试报告",description ="用例执行情况")
 
     runner.run(testunit)#运行测试用例
-    fp.close()#关闭报告文件
+    fp_result.close()#关闭报告文件
