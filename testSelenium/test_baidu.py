f9408a1ca7dc334d2bbc72a3ee12722b8dc0d204
batmangoodsen <1281198374@qq.com>
2017-12-01 09:33:21 +0800

    修改2017/11/30困扰一晚上的问题！！！，if__name__ == "__main__"都要缩进！！！

--- testSelenium/test_baidu.py
+++ testSelenium/test_baidu.py
@@ -1,8 +1,6 @@
-# -*- coding:utf-8 -*-
 from selenium import webdriver
-import unittest
+import unittest,re,time
 import HTMLTestRunner
-import time
 
 class Baidu(unittest.TestCase):
     def setUp(self):
@@ -20,14 +18,15 @@ class Baidu(unittest.TestCase):
         self.driver.quit()
 if __name__ == "__main__":
     testunit = unittest.TestSuite()
-
     testunit.addTest(Baidu("test_baidu_search"))
 
-#定义报告存放路径
-file_path = open(r'E:\py3','wb')
-#file_result = open(file_path ,'wb')
-#定义测试报告
-runner = HTMLTestRunner.HTMLTestRunner(stream=file_path,title="百度搜索测试报告",description ="用例执行情况")
 
-runner.run(testunit)#运行测试用例
-file_path.close()#关闭报告文件
+    #定义报告存放路径
+    fp = open(r'E:\py3','wb')
+    #file_result = open(file_path ,'wb')
+
+    #定义测试报告
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title="百度搜索测试报告",description ="用例执行情况")
+
+    runner.run(testunit)#运行测试用例
+    fp.close()#关闭报告文件
