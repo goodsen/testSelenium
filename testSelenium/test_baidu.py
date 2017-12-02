d6cdba264c86ec4142e3cf852b7a99be71386bd2
goodsen <18618411965@163.com>
2017-12-02 20:50:20 +0800

    1.增加测试报告用例名称（doc string）'''百度搜索测试/搜索关键字''' 2.引入time模块，time.strftime()用来获得当前时间，可以将时间格式化为字符串，避免忘记修改文件名称，覆盖测试用例

--- testSelenium/test_baidu.py
+++ testSelenium/test_baidu.py
@@ -5,6 +5,7 @@ import HTMLTestRunner
 
 #定义百度测试类
 class Baidu(unittest.TestCase):
+    '''百度搜索测试'''
     #启动方法（配置浏览器、url、隐藏等待时间）
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -12,6 +13,7 @@ class Baidu(unittest.TestCase):
         self.base_url = "http://www.baidu.com"
     #定义搜索方法
     def test_baidu_search(self):
+        '''搜索关键字:HTMLTestRunner'''
         driver = self.driver
         driver.get(self.base_url)
         driver.find_element_by_id("kw").send_keys("HTMLTestRunner")
@@ -25,14 +27,18 @@ class Baidu(unittest.TestCase):
 if __name__ == "__main__":
     testunit = unittest.TestSuite()
     testunit.addTest(Baidu("test_baidu_search"))
+#按照一定格式获取当前时间
+    now = time.strftime("%Y-%m-%d %H_%M_%S")
 
 
     #定义报告存放路径
-    fp = "E:\\tes.html"
+    fp = "E:\\" + now + "tes.html"
     fp_result = open(fp ,"wb")
 
     #定义测试报告
-    runner = HTMLTestRunner.HTMLTestRunner(stream=fp_result,title="百度搜索测试报告",description ="用例执行情况")
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp_result,
+                                           title="百度搜索测试报告",
+                                           description ="用例执行情况")
 
     runner.run(testunit)#运行测试用例
     fp_result.close()#关闭报告文件
