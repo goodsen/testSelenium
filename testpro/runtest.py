8269456d67540270b4b82f03905e784508384f59
zhaos <18618411965@163.com>
2017-12-03 21:02:48 +0800

    修改suite.addTest(testsub.TestSub())bug，将"test_add"改成"test_sub"

--- testpro/runtest.py
+++ testpro/runtest.py
@@ -1,4 +1,4 @@
-import  unittest
+import unittest
 
 #加载测试文件
 from testpro import testadd
@@ -6,11 +6,12 @@ from testpro import testsub
 
 #构造测试集
 suite = unittest.TestSuite()
+
 suite.addTest(testadd.TestAdd("test_add"))
 suite.addTest(testadd.TestAdd("test_add2"))
 
-suite.addTest(testsub.TestSub("test_add"))
-suite.addTest(testsub.TestSub("test_add2"))
+suite.addTest(testsub.TestSub("test_sub"))
+suite.addTest(testsub.TestSub("test_sub2"))
 
 if __name__ == '__main__':
     #执行测试
