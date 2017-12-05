f303ee1e46d9190273a08bc0c12729f4f1c081e7
batmangoodsen <1281198374@qq.com>
2017-12-05 14:59:11 +0800

    邮件增加附件失败，回滚到上一版本

--- testpro/send_mail.py
+++ testpro/send_mail.py
@@ -1,8 +1,8 @@
-import smtplib
+import smtplib,os
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
 from email.utils import formataddr
 from email.mime.multipart import MIMEMultipart #发送带附件的邮件
+from email import encoders
 
 my_sender='1281198374@qq.com' #发件人邮箱账号
 my_pass='vhmgggamjmtkhfhe'    #发件人邮箱密码,密码有可能过期，需要在qq查询密码
@@ -16,25 +16,6 @@ def mail():
         msg['To']=formataddr(["hadey",my_user])      #括号里的对应收件人邮箱昵称、收件人邮箱账号
         msg['Subject']="自动化测试"                  #邮件的主题,也可以说是标题
 
-        #邮件正文是MIMETest
-       # msg.attach(MIMEText('本邮件为自动化测试','plain','utf-8'))
-
-        #添加附件就是加上一个MIMEBase,从本地选取文件
-        with open('E:\\image.png','rb') as f:
-            #设置附件的MIME和文件名，这里是html
-            mime = MIMEBase('image','png', filename = 'image.png')
-
-        #加上必要的头信息:
-        mime.add_header('accept','text/html,application/xhtml+xml,'
-                        'application/xml;q=0.9,image/webp,'
-                        'image/apng,*/*;q=0.8')
-        mime.add_header('user-agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                                     'AppleWebKit/537.36 (KHTML, like Gecko)'
-                                     ' Chrome/62.0.3202.94 Safari/537.36')
-        #把附件的内容读进来
-        mime.set_payload(f.read())
-        #添加到MIMEMultipaty
-        msg.attach(mime)
 
         #设置发件人邮件信息
         server=smtplib.SMTP_SSL("smtp.qq.com",465)  #发件人邮箱中的SMTP服务器，端口是25
